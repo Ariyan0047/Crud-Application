6b9204e9ba75b11f52fa34135b7e15a97e4347d5
Ariyan0047 <aariyan0047@gmail.com>
2021-04-13 17:57:49 +0600

    getting data from database

--- templates/data.php
+++ templates/data.php
@@ -1,3 +1,9 @@
+<?php
+$statement = $conn->prepare("SELECT * FROM products ORDER BY id");
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
 <div class="container mt-5 mb-5">
     <table class="table table-striped">
         <thead>
@@ -9,12 +15,16 @@
             </tr>
         </thead>
         <tbody>
+            <?php foreach ($products as $product): ?>
             <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <th scope="row"><?php echo htmlspecialchars(
+                  $product["id"]
+                ); ?></th>
+                <td><?php echo htmlspecialchars($product["title"]); ?></td>
+                <td><?php echo htmlspecialchars($product["prize"]); ?></td>
+                <td><?php echo htmlspecialchars($product["created_at"]); ?></td>
             </tr>
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
